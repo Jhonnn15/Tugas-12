922fb54aeb7bfa692a2f0c4cdb9cc7dd0fec0d44
Bakti Ihlasul <161696621+Jhonnn15@users.noreply.github.com>
2024-06-08 22:15:50 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+
+from text import count_upper_case_letters
+
+text = "Hai Perkenalkan Nama Saya Budiono Siregar Cita-cita Saya Kapal Laut."
+
+upper_case_count = count_upper_case_letters(text)
+
+print(f"Jumlah huruf besar dalam teks: {upper_case_count}")
